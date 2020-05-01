51a332fa127b38d9f8ab3bb0ee92c437bf5de636
片岡祐太郎 <hitugaya12@gmail.com>
2020-05-01 23:50:30 +0900

    implement url-loader and file-loader

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,14 @@ module.exports = {
         test: /\.css$/,
         use: ["style-loader", "css-loader"], // loaderは逆順（後ろから順）に読み込まれる。順番に注意しないとコンパイルエラー
       },
+      {
+        test: /\.(jpe?g|png|gif|svg|ico)$/i,
+        loader: "url-loader", // 2kb以下だとBase64で読み込む
+        options: {
+          limit: 2048, // 2kb以上だとfile-loaderで読み込む(別ファイルとして読み込まれる)
+          name: "./images/[name].[ext]",
+        },
+      },
     ],
   },
   devServer: {

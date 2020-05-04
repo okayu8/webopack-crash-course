d7affdffd44d7152a82d706262ac8c992da0cfe1
片岡祐太郎 <hitugaya12@gmail.com>
2020-05-05 00:58:38 +0900

    apply uglifyjs-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+// const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 
 const outputPath = path.resolve(__dirname, "dist");
 
@@ -43,4 +44,16 @@ module.exports = {
       filename: "[name].[hash].css",
     }),
   ],
+  // うまく動かないためコメントアウト（uglifyjsによるconsole.log自動削除）
+  // optimization: {
+  //   minimizer: [
+  //     new UglifyJsPlugin({
+  //       uglifyOptions: {
+  //         compress: {
+  //           drop_console: true,
+  //         },
+  //       },
+  //     }),
+  //   ],
+  // },
 };
